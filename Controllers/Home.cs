2e24715c0c1f13fa8447a001fea36bb3996c5409
Mubashar Yasin <mubasharraj012@gmail.com>
2025-09-15 23:16:13 +0500

    Lecture#_23
    Showing data in shared layout from two different models

--- Controllers/Home.cs
+++ Controllers/Home.cs
@@ -4,28 +4,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blog_Site.Controllers
 {
-  
-    public class Home : Controller 
+    public class Home : Controller
     {
 
-        AppDbContext db; 
+        AppDbContext db;
 
-        public Home(AppDbContext _db) 
+        public Home(AppDbContext _db)
         {
-            db= _db;
+            db = _db;
         }
         public IActionResult Index()
         {
+            SharedLayoutData();
             IEnumerable<Post> myPost = db.tbl_Post;
             return View(myPost);
         }
 
-        [Route("Home/Post/{slug}")] 
+        [Route("Home/Post/{slug}")]
         public IActionResult Post(string slug)
         {
-            
-            var DetailedPost = db.tbl_Post.Where(x => x.slug == slug).FirstOrDefault(); 
+            SharedLayoutData();
+            var DetailedPost = db.tbl_Post.Where(x => x.slug == slug).FirstOrDefault();
             return View(DetailedPost);
+        }
+
+        public void SharedLayoutData()
+        {
+            ViewBag.Post = db.tbl_Post.ToList();
+            ViewBag.Profile = db.tbl_Profile.FirstOrDefault();
         }
     }
 }
